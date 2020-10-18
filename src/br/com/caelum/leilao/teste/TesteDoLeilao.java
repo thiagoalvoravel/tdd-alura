b56015f19b63d130ca8facbfa4d21c562d6d26f9
Thiago Alvoravel <t.alvoravel@gmail.com>
2020-10-18 10:16:42 -0300

    Inclusão de testes para classe Leilao

---
+++ src/br/com/caelum/leilao/teste/TesteDoLeilao.java
@@ -0,0 +1,36 @@
+package br.com.caelum.leilao.teste;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Usuario;
+
+public class TesteDoLeilao {
+
+	@Test
+	public void deveReceberUmLance() {
+		Leilao leilao = new Leilao("MacBook Air");
+		assertEquals(0, leilao.getLances().size());
+		
+		leilao.propoe(new Lance(new Usuario("Paula"), 2000));
+		assertEquals(1, leilao.getLances().size());
+		assertEquals(2000.0, leilao.getLances().get(0).getValor(), 0.00001);
+	}
+	
+	@Test
+	public void deveReceberVariosLances() {
+		Leilao leilao = new Leilao("MacBook Air");
+		assertEquals(0, leilao.getLances().size());
+		
+		leilao.propoe(new Lance(new Usuario("Paula"), 2000));
+		leilao.propoe(new Lance(new Usuario("Vitor"), 3000));
+		
+		assertEquals(2, leilao.getLances().size());
+		assertEquals(2000.0, leilao.getLances().get(0).getValor(), 0.00001);
+		assertEquals(3000.0, leilao.getLances().get(1).getValor(), 0.00001);
+	}
+	
+}
